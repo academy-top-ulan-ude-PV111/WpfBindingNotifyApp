4c83fab6effae37ed658fca70dcdd3f2abbf07c8
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-18 16:23:35 +0300

    Добавьте файлы проекта.

---
+++ WpfBindingNotifyApp/DataSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBindingNotifyApp
+{
+    internal class DataSource : INotifyPropertyChanged
+    {
+        private int value = 1;
+
+        public int Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                OnPropertyChanged(nameof(this.Value));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+            PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
---
+++ WpfBindingNotifyApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfBindingNotifyApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new DataSource();
+
+            timer = new() { Interval = TimeSpan.FromMilliseconds(500) };
+            timer.Tick += Timer_Tick;
+
+            timerValue.Text = (DataContext as DataSource).Value.ToString();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            (DataContext as DataSource).Value++;
+            timerValue.Text = (DataContext as DataSource).Value.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+                timer.Start();
+        }
+    }
+}
